cb481c8cd3bb8b49aac2d2b91d18971691248dfb
Brendan Devenney <brendan@devenney.io>
2017-05-08 00:17:39 +0100

    Invoice Outstanding Totals (#3)
    * Adds functionality to track outstanding balances.
    * Adds functionality to mark invoices paid or unpaid.
    * Slight UI improvements.

--- app/routes/invoices.js
+++ app/routes/invoices.js
@@ -8,6 +8,20 @@ export default Ember.Route.extend({
       if (confirmation) {
         invoice.destroyRecord();
       }
+    },
+
+    markInvoicePaid(invoice) {
+      invoice.paid = true;
+      invoice.save();
+    },
+
+    markInvoiceUnpaid(invoice) {
+      let confirmation = confirm('Mark invoice unpaid?');
+
+      if (confirmation) {
+        invoice.paid = false;
+        invoice.save();
+      }
     }
   }
 });
--- app/routes/invoices/view.js
+++ app/routes/invoices/view.js
@@ -2,16 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return Ember.RSVP.hash({
-      invoice: this.store.findRecord('invoice', params.invoice_id),
-      customers: this.store.findAll('customer')
-    })
+    return this.store.findRecord('invoice', params.invoice_id)
   },
 
   setupController: function(controller, model) {
     this._super(controller, model);
 
-    controller.set('invoice', model.invoice);
+    controller.set('invoice', model);
+
     controller.set('title', 'Edit invoice');
     controller.set('buttonLabel', 'Save changes');
   },
@@ -24,7 +22,7 @@ export default Ember.Route.extend({
     printInvoice() {
       // TODO: Wait for result.
       window.print();
-    }
+    },
   }
 });
 
