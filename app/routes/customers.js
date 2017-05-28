45c5181e0a8622b4db0f4bf0640adc7b380dc4f7
Brendan Devenney <brendan@devenney.io>
2017-05-28 22:13:39 +0100

    Force preloading of all items and invoices in customer views

--- app/routes/customers.js
+++ app/routes/customers.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  afterModel: function() {
+    var store = this.get('store')
+
+    var promises = [
+      store.findAll('invoice-line'),
+      store.findAll('invoice'),
+      store.findAll('customer')
+    ]
+
+    return Ember.RSVP.all(promises);
+  },
 });
