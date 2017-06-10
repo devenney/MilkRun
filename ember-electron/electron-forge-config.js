685670b4e63e7b89334da78d55062b24598dc62b
Brendan Devenney <brendan@devenney.io>
2017-06-10 22:51:09 +0100

    Electron Packaging (#11)

---
+++ ember-electron/electron-forge-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "make_targets": {
+    "win32": [
+      "squirrel"
+    ],
+  },
+  "electronPackagerConfig": {},
+  "electronWinstallerConfig": {
+    "name": "MilkRun"
+  },
+  "electronInstallerDebian": {},
+  "electronInstallerRedhat": {},
+  "github_repository": {
+    "owner": "",
+    "name": ""
+  },
+  "windowsStoreConfig": {
+    "packageName": "MilkRun"
+  }
+};
